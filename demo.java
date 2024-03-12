abbe1c7d73000de5fa856b913f0b48300b846f5b
ademersseman <alan.demersseman@gmail.com>
2024-03-11 23:15:28 -0400

    test

--- demo.java
+++ demo.java
@@ -1,32 +1,73 @@
-import java.awt.*;
- 
-class button {
-    button()
-    {
-        Frame f = new Frame();
- 
-        // Button 1 created
-        // OK button
-        Button b1 = new Button("OK");
-        b1.setBounds(100, 50, 50, 50);
-        f.add(b1);
- 
-        // Button 2 created
-        // Submit button
-        Button b2 = new Button("SUBMIT");
-        b2.setBounds(100, 101, 50, 50);
-        f.add(b2);
-       
-        // Button 3 created
-        // Cancel button
-        Button b3 = new Button("CANCEL");
-        b3.setBounds(100, 150, 80, 50);
-        f.add(b3);
- 
-        f.setSize(500, 500);
-        f.setLayout(null);
-        f.setVisible(true);
-    }
- 
-    public static void main(String a[]) { new button(); }
+// Java program to create a blank text 
+// field of definite number of columns.
+import java.awt.event.*;
+import javax.swing.*;
+class text extends JFrame implements ActionListener {
+	// JTextField
+	static JTextField t;
+
+	// JFrame
+	static JFrame f;
+
+	// JButton
+	static JButton b;
+
+	// label to display text
+	static JLabel l;
+
+	// default constructor
+	text()
+	{
+	}
+
+	// main class
+	public static void main(String[] args)
+	{
+		// create a new frame to store text field and button
+		f = new JFrame("textfield");
+
+		// create a label to display text
+		l = new JLabel("nothing entered");
+
+		// create a new button
+		b = new JButton("submit");
+
+		// create a object of the text class
+		text te = new text();
+
+		// addActionListener to button
+		b.addActionListener(te);
+
+		// create a object of JTextField with 16 columns
+		t = new JTextField(16);
+
+		// create a panel to add buttons and textfield
+		JPanel p = new JPanel();
+
+		// add buttons and textfield to panel
+		p.add(t);
+		p.add(b);
+		p.add(l);
+
+		// add panel to frame
+		f.add(p);
+
+		// set the size of frame
+		f.setSize(300, 300);
+
+		f.show();
+	}
+
+	// if the button is pressed
+	public void actionPerformed(ActionEvent e)
+	{
+		String s = e.getActionCommand();
+		if (s.equals("submit")) {
+			// set the text of the label to the text of the field
+			l.setText(t.getText());
+
+			// set the text of field to blank
+			t.setText(" ");
+		}
+	}
 }
